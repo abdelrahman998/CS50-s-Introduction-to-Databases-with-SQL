f8422283c1c250d155ac47e2aee7636354d8d66a
abdelrahman998 <a.alesawy98@gmail.com>
2024-04-01 22:32:31 +0200

    Add Relating Problem Set

--- 1.moneyball/7.sql
+++
@@ -1,6 +0,0 @@
-SELECT "first_name", "last_name" FROM "players"
-WHERE "id" = (
-    SELECT "player_id" FROM "salaries"
-    ORDER BY "salaries" DESC
-    LIMIT 1
-);
--- moneyball/4.sql
+++
@@ -1,8 +0,0 @@
-SELECT "first_name", "last_name", "salary"
-    FROM "players"
-JOIN "salaries" ON "players"."id" = "salaries"."player_id"
-    WHERE "year" = 2001
-    ORDER BY "salary" ASC,
-             "players"."first_name" ASC,
-             "players"."last_name" ASC
-    LIMIT 20;
